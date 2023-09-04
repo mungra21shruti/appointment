9c3e2b190badecfdefd0dfc5b47e17f18b7889b0
05 <nilsavani12@gmail.com>
2023-09-04 18:12:46 +0530

    1

--- lib/first.dart
+++ lib/first.dart
@@ -1,4 +1,8 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:local_auth/local_auth.dart';
+import 'package:local_auth/error_codes.dart' as auth_error;
 
 class first extends StatefulWidget {
   const first({super.key});
@@ -8,8 +12,41 @@ class first extends StatefulWidget {
 }
 
 class _firstState extends State<first> {
+  final LocalAuthentication auth = LocalAuthentication();
+
+  @override
+  Future<void> initState() async {
+    // TODO: implement initState
+    super.initState();
+    final bool canAuthenticateWithBiometrics = await auth.canCheckBiometrics;
+    final bool canAuthenticate =
+        canAuthenticateWithBiometrics || await auth.isDeviceSupported();
+
+    try {
+      final bool didAuthenticate = await auth.authenticate(
+          localizedReason: 'Please authenticate to show account balance',
+          options: const AuthenticationOptions(useErrorDialogs: false));
+    } on PlatformException catch (e) {
+      if (e.code == auth_error.notAvailable) {
+        // Add handling of no hardware here.
+      } else if (e.code == auth_error.notEnrolled) {
+      } else {}
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: Text("realtime database")),
+      body: Column(
+        children: [
+          FloatingActionButton(
+            backgroundColor: Colors.transparent,
+            child: Icon(Icons.fingerprint_outlined),
+            onPressed: () async {},
+          ),
+        ],
+      ),
+    );
   }
 }
